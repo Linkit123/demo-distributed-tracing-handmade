5403c04b6f40ac255947ff2a51adc22e1623dc43
link2mountain <linhtn@vnpay.vn>
2022-01-03 17:18:00 +0700

    update

--- animal-name/src/main/java/com/dvtt/demo/distributedtracinghandmade/animalname/AnimalNameApplication.java
+++ animal-name/src/main/java/com/dvtt/demo/distributedtracinghandmade/animalname/AnimalNameApplication.java
@@ -2,8 +2,10 @@ package com.dvtt.demo.distributedtracinghandmade.animalname;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.dvtt.demo")
 public class AnimalNameApplication {
 
 	public static void main(String[] args) {
--- animal-name/src/main/java/com/dvtt/demo/distributedtracinghandmade/animalname/service/AnimalServiceImpl.java
+++ animal-name/src/main/java/com/dvtt/demo/distributedtracinghandmade/animalname/service/AnimalServiceImpl.java
@@ -1,11 +1,16 @@
 package com.dvtt.demo.distributedtracinghandmade.animalname.service;
 
-import org.springframework.core.io.ClassPathResource;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -14,21 +19,35 @@ import java.util.stream.Collectors;
  * Created by linhtn on 1/2/2022.
  */
 @Service
+@AllArgsConstructor
 public class AnimalServiceImpl implements AnimalService {
 
-    private final List<String> animalNames;
-    private final Random random;
-
-    public AnimalServiceImpl() throws IOException {
-        var inputStream = new ClassPathResource("static/animals.txt").getInputStream();
-        try (var reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            animalNames = reader.lines().collect(Collectors.toList());
-        }
-        this.random = new Random();
-    }
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public String getName() {
-        return animalNames.get(random.nextInt(animalNames.size()));
+        var random = new Random();
+        var url = "https://61d2c38bb4c10c001712b58a.mockapi.io/api/v1/animals";
+        var s = httpGet(url);
+        try {
+            var listAnimals = objectMapper.readValue(s, new TypeReference<List<HashMap<String, String>>>() {
+            });
+            var animalNames = listAnimals.stream().map(x -> x.get("name")).collect(Collectors.toList());
+            return animalNames.get(random.nextInt(animalNames.size()));
+        } catch (Exception ex) {
+            return "";
+        }
+
     }
+
+    private String httpGet(String url) {
+        var uri = UriComponentsBuilder.fromHttpUrl(url)
+                .build().toString();
+        var headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT_LANGUAGE, "vi-VN");
+        var exchange = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        return exchange.getBody();
+    }
+
 }
