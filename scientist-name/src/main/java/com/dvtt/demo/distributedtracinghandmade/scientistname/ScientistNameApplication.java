b5736d75009c9a5018c1253727edcfb411964d0e
link2mountain <linhtn@vnpay.vn>
2022-01-03 17:18:33 +0700

    update

--- scientist-name/src/main/java/com/dvtt/demo/distributedtracinghandmade/scientistname/ScientistNameApplication.java
+++ scientist-name/src/main/java/com/dvtt/demo/distributedtracinghandmade/scientistname/ScientistNameApplication.java
@@ -2,8 +2,10 @@ package com.dvtt.demo.distributedtracinghandmade.scientistname;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.dvtt.demo")
 public class ScientistNameApplication {
 
 	public static void main(String[] args) {
